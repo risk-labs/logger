6b28bbc87bf6b2fc6fcc08ba10806c5b3a296922
Ihor Farion <65650773+grasphoper@users.noreply.github.com>
2025-09-25 15:17:49 -0700

    fix: remove runtime dependency on `web3-utils` (#5)
    * remove web3-utils dependency and exports
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>
    
    * bump version
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>
    
    ---------
    
    Signed-off-by: Ihor Farion <ihor@umaproject.org>

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,3 @@
-import type { AbiItem, toBN } from "web3-utils";
-
-export type Abi = AbiItem | AbiItem[];
-
-export type BN = ReturnType<typeof toBN>;
-
 export type Awaited<T> = T extends PromiseLike<infer U> ? Awaited<U> : T;
 
 export type FinancialContractType = "ExpiringMultiParty" | "Perpetual";
